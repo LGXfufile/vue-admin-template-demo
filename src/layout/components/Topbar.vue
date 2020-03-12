360af8696cbbb151edb58f0284ed24770115a0ab
alone <351030015@qq.com>
2020-03-12 10:55:09 +0800

    项目初始化

---
+++ src/layout/components/Topbar.vue
@@ -0,0 +1,141 @@
+<template>
+  <div class="top-nav">
+    <div class="log">工地可视化管理系统</div>
+    <el-menu :default-active="activeMenu" mode="horizontal" :active-text-color="variables.menuActiveText" @select="handleSelect">
+      <div v-for="item in routes" :key="item.path" class="nav-item">
+        <div v-if="isOnlyOneChild(item)">
+          <app-link :to="resolvePath(item)">
+            <el-menu-item v-if="!item.hidden" :index="item.path">{{ item.meta ? item.meta.title : item.children[0].meta.title }}
+            </el-menu-item>
+          </app-link>
+        </div>
+        <div v-else>
+          <el-menu-item v-if="!item.hidden" :index="item.path">{{ item.meta ? item.meta.title : item.children[0].meta.title }}
+          </el-menu-item>
+        </div>
+      </div>
+    </el-menu>
+
+    <div class="right-menu">
+      <el-dropdown class="avatar-container" trigger="click">
+        <div class="avatar-wrapper">
+          <img :src="avatar+'?imageView2/1/w/80/h/80'" class="user-avatar">
+          <i class="el-icon-caret-bottom" />
+        </div>
+        <el-dropdown-menu slot="dropdown" class="user-dropdown">
+          <router-link to="/">
+            <el-dropdown-item>
+              Home
+            </el-dropdown-item>
+          </router-link>
+          <a target="_blank" href="https://github.com/PanJiaChen/vue-admin-template/">
+            <el-dropdown-item>Github</el-dropdown-item>
+          </a>
+          <a target="_blank" href="https://panjiachen.github.io/vue-element-admin-site/#/">
+            <el-dropdown-item>Docs</el-dropdown-item>
+          </a>
+          <el-dropdown-item divided @click.native="logout">
+            <span style="display:block;">Log Out</span>
+          </el-dropdown-item>
+        </el-dropdown-menu>
+      </el-dropdown>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+import AppLink from './Sidebar/Link'
+import { constantRoutes } from '@/router'
+import variables from '@/styles/variables.scss'
+
+export default {
+  name: 'Topbar',
+  components: {
+    AppLink
+  },
+  data() {
+    return {
+      routes: constantRoutes
+    }
+  },
+  computed: {
+    activeMenu() {
+      const route = this.$route
+      const { meta, path } = route
+      // if set path, the sidebar will highlight the path you set
+      if (meta.activeMenu) {
+        return meta.activeMenu
+      }
+      // 如果是首页，首页高亮
+      if (path === '/dashboard') {
+        return '/'
+      }
+      // 如果不是首页，高亮一级菜单
+      const activeMenu = '/' + path.split('/')[1]
+      return activeMenu
+    },
+    variables() {
+      return variables
+    },
+    sidebar() {
+      return this.$store.state.app.sidebar
+    },
+    ...mapGetters([
+      'avatar'
+    ])
+  },
+  mounted() {
+    this.initCurrentRoutes()
+  },
+  methods: {
+    // 通过当前路径找到二级菜单对应项，存到store，用来渲染左侧菜单
+    initCurrentRoutes() {
+      const { path } = this.$route
+      let route = this.routes.find(item => item.path === '/' + path.split('/')[1])
+      // 如果找不到这个路由，说明是首页
+      if (!route) {
+        route = this.routes.find(item => item.path === '/')
+      }
+      this.$store.commit('permission/SET_CURRENT_ROUTES', route)
+      this.setSidebarHide(route)
+    },
+    // 判断该路由是否只有一个子项或者没有子项，如果是，则在一级菜单添加跳转路由
+    isOnlyOneChild(item) {
+      if (item.children && item.children.length === 1) {
+        return true
+      }
+      return false
+    },
+    resolvePath(item) {
+      if (item.path === '/') {
+        const path = '/' + item.children[0].path
+        return path
+      }
+      if (item.children) {
+        const path = item.path + '/' + item.children[0].path
+        return path
+      }
+      return item.path
+    },
+    handleSelect(key, keyPath) {
+      // 把选中路由的子路由保存store
+      const route = this.routes.find(item => item.path === key)
+      this.$store.commit('permission/SET_CURRENT_ROUTES', route)
+      this.setSidebarHide(route)
+    },
+    // 设置侧边栏的显示和隐藏
+    setSidebarHide(route) {
+      if (!route.children || route.children.length === 1) {
+        this.$store.dispatch('app/toggleSideBarHide', true)
+      } else {
+        this.$store.dispatch('app/toggleSideBarHide', false)
+      }
+    },
+    async logout() {
+      await this.$store.dispatch('user/logout')
+      this.$router.push(`/login?redirect=${this.$route.fullPath}`)
+    }
+  }
+}
+</script>
